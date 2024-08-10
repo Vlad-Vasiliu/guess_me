8a273b345a56757b1bd4d5bf3cf202dbbec82b15
Vlad Vasiliu <vlad.vasiliu11@gmail.com>
2024-08-10 11:39:14 -0400

    added SOME styling...frustrating

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let num_guesses = 1
 let actual = Math.floor(Math.random()*100)
 let guesses_list = []
+
 // document.querySelector('#guess').focus()
 
 const play = () => {
@@ -14,11 +15,12 @@ const play = () => {
     console.log(actual)
 
     //reseting DOM elements to proper new game and fousing on input
-    document.querySelector('#guess').focus()
+    document.querySelector('#submit').removeAttribute('hidden')
     document.getElementById('hint').innerHTML='';
     document.querySelector('#play_again').setAttribute('style','display:"none"')
     document.querySelector('#submit').removeAttribute('disabled')
     document.querySelector('#guess').removeAttribute('disabled')
+    document.querySelector('#guess').focus()
 }
 
 const getInfo = (e) =>{
@@ -57,6 +59,7 @@ const getInfo = (e) =>{
             else {
                 document.getElementById('hint').innerHTML=`YOU SHOULD PLAY THE LOTTO, TOO LUCKY!`
             }
+            document.querySelector('#submit').setAttribute('hidden','')
             document.getElementById('play_again').setAttribute('style','display:inline')
     
             return 0;
@@ -74,11 +77,12 @@ const getInfo = (e) =>{
 
 }
 
+// check for invalid inputs like strings or higher than 100, lower than 0
 const validate_input = () => {
     guess = document.querySelector('#guess').value
     if ( isNaN(guess) )  return false;
     if ( Number(guess) > 100 || Number(guess) < 0 ) return false;
-
+    
     return true;
 }
 
